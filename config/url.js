c1b53c258170c377654ceebcf1e39a8d924f6f18
SharathKumarR25 <45233620+SharathKumarR25@users.noreply.github.com>
2019-01-17 15:39:21 +0530

    config

--- config/url.js
+++ config/url.js
@@ -1,7 +1,7 @@
-const booksUrl = 'http://10.10.4.173:4000/api/books/';
-const authorsUrl = 'http://10.10.4.173:4000/api/authors/';
-const loginUrl = 'http://10.10.4.173:5000/api/login/';
-const signUpUrl = 'http://10.10.4.173:5000/api/signup/';
+const booksUrl = 'http://13.233.98.248:4000/api/books/';
+const authorsUrl = 'http://13.233.98.248:4000/api/authors/';
+const loginUrl = 'http://13.233.98.248:5000/api/login/';
+const signUpUrl = 'http://13.233.98.248:5000/api/signup/';
 
 module.exports = {
   booksUrl,
